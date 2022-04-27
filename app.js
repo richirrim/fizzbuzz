51c19f2a902393ad11fd9ac31450bf32e4a4acd0
richirrim <ricardoortega.dev@gmail.com>
2022-04-26 22:16:13 -0500

    Crea el modulo FizzbuzzService.

--- app.js
+++ app.js
@@ -40,13 +40,27 @@
 // console.log(newListExplorersTrick)
 
 const ExplorerService = require('./lib/services/ExplorerService')
+const FizzbuzzService = require('./lib/services/FizzbuzzService')
 const Reader = require('./lib/utils/Reader')
 
 const explorersList = Reader.jsonFile('explorers.json')
+
+console.log('ExplorerService', '------'.repeat(6))
 
 console.log(ExplorerService.filterByMission(explorersList, 'node'))
 console.log(ExplorerService.getAmountOfExplorersByMission(explorersList, 'node'))
 console.log(ExplorerService.getExplorersUsernamesByMission(explorersList, 'node'))
 
-console.log('\n', '------'.repeat(6))
-console.log('------'.repeat(6), '\n')
+console.log('\n', 'FizzbuzzService', '------'.repeat(6), '\n')
+
+const explorer1 = explorersList[0]
+console.log(FizzbuzzService.applyValidationInExplorer(explorer1)) // trick: 1
+
+const explorer3 = explorersList[2]
+console.log(FizzbuzzService.applyValidationInExplorer(explorer3)) // trick: FIZZ
+
+const explorer5 = explorersList[4]
+console.log(FizzbuzzService.applyValidationInExplorer(explorer5)) // trick: BUZZ
+
+const explorer15 = explorersList[14]
+console.log(FizzbuzzService.applyValidationInExplorer(explorer15)) // trick: FIZZBUZZ
