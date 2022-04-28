56bc8b07b5ff7e421873312f5db799a1e7446967
richirrim <ricardoortega.dev@gmail.com>
2022-04-28 00:26:13 -0500

    Crea archivo de pruebas y añade una prueba de ejemplo para hacer peticion GET que devuelve un 200

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -4,7 +4,7 @@ const request = require('supertest')
 describe('GET /', () => {
   it('Deberia responder con un código 200', async () => {
     const response = await request(server).get('/msj').send()
-
+    
     expect(response.statusCode).toBe(200)
   })
 })
