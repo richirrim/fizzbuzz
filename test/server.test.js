91953d8b00f05d48cfc6d4239390443bc7a75f37
richirrim <ricardoortega.dev@gmail.com>
2022-04-29 22:00:10 -0500

    Crea test para la ruta '/v1/fizzbuzz/:score' y la funcionalidad para dicha petición.

--- test/server.test.js
+++ test/server.test.js
@@ -30,18 +30,31 @@ describe('GET /v1/explorers/amount/node', () => {
     expect(mission).toBe('node')
     expect(quantity).not.toBeNull()
   })
+})
 
-  describe('GET /v1/explorers/usernames/node', () => {
-    it('Deberia devolver una lista de los usernames de los explorers en la Mission Node', async () => {
-      const response = await api.get('/v1/explorers/usernames/node').send()
-      const { mission, usernamesList } = response.body
+describe('GET /v1/explorers/usernames/node', () => {
+  it('Deberia devolver una lista de los usernames de los explorers en la Mission Node', async () => {
+    const response = await api.get('/v1/explorers/usernames/node').send()
+    const { mission, usernamesList } = response.body
 
-      expect(response.status).toBe(200)
-      expect(response.type).toEqual(expect.stringContaining('json'))
-      expect(response.body).toBeInstanceOf(Object)
-      expect(mission).toBe('node')
-      expect(usernamesList).not.toHaveLength(0)
-    })
+    expect(response.status).toBe(200)
+    expect(response.type).toEqual(expect.stringContaining('json'))
+    expect(response.body).toBeInstanceOf(Object)
+    expect(mission).toBe('node')
+    expect(usernamesList).not.toHaveLength(0)
+  })
+})
+
+describe('GET /v1/fizzbuzz/:score', () => {
+  it('Deberia devolver un objeto con la propiedad trick y dependiedo el caso con el valor fizz, buzz, fizzbuzz o un valor númerico.', async () => {
+    const response = await api.get('/v1/fizzbuzz/:score').send()
+    const { score, trick } = response.body
+
+    expect(response.status).toBe(200)
+    expect(response.type).toEqual(expect.stringContaining('json'))
+    expect(response.body).toBeInstanceOf(Object)
+    expect(score).not.toBeNull()
+    expect(trick).not.toBeNull()
   })
 })
 
